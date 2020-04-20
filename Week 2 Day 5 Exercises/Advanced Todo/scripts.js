79b72ad1e170d0ffc943be653f8c62b37d9a3d4d
peterhero <63244012+peterhero@users.noreply.github.com>
2020-04-20 14:47:04 +0200

    updated advanced todo

--- Week 2 Day 5 Exercises/Advanced Todo/scripts.js
+++ Week 2 Day 5 Exercises/Advanced Todo/scripts.js
@@ -1,6 +1,7 @@
 $('button').click(() => {
 	$('.todos').append(`<div class="todos-container">${$('input').val()}<img src="trash.png" alt="trash" class="delete"></img><img src="check1.png" alt="check" class="check"></img></div>`);
-	
+	$('input').val("");
+
 	$('.todos').css('font-family', `'Manrope', sans-serif`);
 	$('.todos').css('font-weight', 'bold');
 	$('.todos').css('letter-spacing', '1px');
